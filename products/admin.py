0c5db67ccfa645db63c75cdb486fb5c30abf765e
SaltaK <s2103k@gmail.com>
2021-07-06 17:37:05 +0600

    homework

---
+++ products/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from products.models import *
+
+
+class ProductAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Products
+    search_fields = ['title', 'descriptions']
+    list_display = 'id title category price'.split()
+    list_filter = 'category'.split()
+
+
+admin.site.register(Category)
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Review)
